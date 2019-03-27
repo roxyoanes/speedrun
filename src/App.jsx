90ec525ab10c2a5edb5ef88f40d46af7af2f48a0
roxioanes <roxioanes11@gmail.com>
2019-03-27 15:41:17 +0100

    deleting initial html

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,6 @@ import Routes from './Routes';
 const App = () => (
   <BrowserRouter>
     <main className="container">
-      <div>
-        <h1>hello world!</h1>
-        <p>If you see this everything is working!</p>
-      </div>
-      <ul className="left" />
       <Routes />
     </main>
   </BrowserRouter>
