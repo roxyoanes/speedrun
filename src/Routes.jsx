e50e2b66cc3a9622485395a2ab57e9995f25d8b2
roxioanes <roxioanes11@gmail.com>
2019-04-16 15:51:21 +0200

    second page project

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Home } from './containers';
+import { Home, SecondPage } from './containers';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/page-2" component={SecondPage} />
   </Switch>
 );
 
---
+++ src/containers/SecondPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const SecondPage = () => (
+  <div className="container">
+    <div className="navbar">
+      <a className="Start" href="#top">Start Bootstrap</a>
+      <div className="right-side">
+        <ul className="right-side__navbar-elements">
+          <li>SERVICES</li>
+          <li>PORTFOLIO</li>
+          <li>ABOUT</li>
+          <li>TEAM</li>
+          <li>CONTACT</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+);
+
+export default SecondPage;
