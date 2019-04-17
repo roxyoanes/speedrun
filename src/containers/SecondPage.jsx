4f08d2e4de9b60a8d2a3143d065a24590dd2812c
roxioanes <roxioanes11@gmail.com>
2019-04-17 10:09:47 +0200

    button class added

--- src/containers/SecondPage.jsx
+++ src/containers/SecondPage.jsx
@@ -3,17 +3,19 @@ import React from 'react';
 import '../styles/second-page.scss';
 
 const SecondPage = () => (
-  <div className="container">
-    <div className="navbar">
-      <a className="Start" href="#top">Start Bootstrap</a>
-      <div className="right-side">
-        <ul className="right-side__navbar-elements">
-          <li>SERVICES</li>
-          <li>PORTFOLIO</li>
-          <li>ABOUT</li>
-          <li>TEAM</li>
-          <li>CONTACT</li>
-        </ul>
+  <div className="secondpage">
+    <div className="container">
+      <div className="navbar">
+        <a className="start" href="#top">Start Bootstrap</a>
+        <div className="navbar__right-side">
+          <nav className="navbar__elements">
+            <button className="button" type="button">SERVICES</button>
+            <button className="button" type="button">PORTFOLIO</button>
+            <button className="button" type="button">ABOUT</button>
+            <button className="button" type="button">TEAM</button>
+            <button className="button" type="button">CONTACT</button>
+          </nav>
+        </div>
       </div>
     </div>
   </div>
