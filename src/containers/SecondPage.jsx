895bc5e55796cf1cedc2d5b6f1bb6c7e12616ece
roxioanes <roxioanes11@gmail.com>
2019-04-17 11:39:44 +0200

    card services

--- src/containers/SecondPage.jsx
+++ src/containers/SecondPage.jsx
@@ -18,9 +18,37 @@ const SecondPage = () => (
         </div>
       </div>
       <div className="landing-page">
-        <h1 className="landing-page__welcome">Welcome To Our Studio!</h1>
-        <h2 className="landing-page__subtext">IT'S NICE TO MEET YOU</h2>
+        <h2 className="landing-page__welcome">Welcome To Our Studio!</h2>
+        <h3 className="landing-page__subtext">IT'S NICE TO MEET YOU</h3>
         <button className="btn-yellow" type="button">TELL ME MORE</button>
+      </div>
+      <div className="services-page">
+        <h2 className="services-page__title">SERVICES</h2>
+        <h1 className="services-page__sub-title">Lorem ipsum dolor sit amet consectetur.</h1>
+        <div className="card-icon">
+          <span className="fa-stack fa-2x">
+            <i className="fas fa-circle fa-stack-2x" />
+            <i className="fas fa-shopping-cart fa-stack-1x fa-inverse" />
+          </span>
+          <h4 className="card-icon__icons-title">E-Commerce</h4>
+          <p className="card-icon__p-card">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+        </div>
+        <div className="card-icon">
+          <span className="fa-stack fa-2x">
+            <i className="fas fa-circle fa-stack-2x" />
+            <i className="fas fa-laptop fa-stack-1x fa-inverse" />
+          </span>
+          <h4 className="card-icon__icons-title">Responsive Design</h4>
+          <p className="card-icon__p-card">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+        </div>
+        <div className="card-icon">
+          <span className="fa-stack fa-2x">
+            <i className="fas fa-circle fa-stack-2x" />
+            <i className="fas fa-lock fa-stack-1x fa-inverse" />
+          </span>
+          <h4 className="card-icon__icons-title">Web Security</h4>
+          <p className="card-icon__p-card">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+        </div>
       </div>
     </div>
   </div>
