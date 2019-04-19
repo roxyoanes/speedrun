c505f80c9e5655b5096dc6c3ee72b574fbe39428
roxioanes <roxioanes11@gmail.com>
2019-04-19 16:15:37 +0200

    images imported and last circle

--- src/containers/SecondPage.jsx
+++ src/containers/SecondPage.jsx
@@ -9,6 +9,10 @@ import pic3 from './images/pic3.jpg';
 import pic4 from './images/04.jpg';
 import pic5 from './images/05.jpg';
 import pic6 from './images/06.jpg';
+import mountain from './images/mountain.jpg';
+import airplane from './images/airplane.jpg';
+import laptop from './images/laptop.jpg';
+import mountain2 from './images/mountain2.jpg';
 
 const SecondPage = () => (
   <div className="secondpage">
@@ -102,6 +106,9 @@ const SecondPage = () => (
         <div className="timeline">
           <div className="container1">
             <div className="timeline__content-1">
+              <div className="timeline__circle">
+                <img src={mountain} className="timeline__img" alt="" />
+              </div>
               <h4>2009-2011</h4>
               <h3>Our Humble Beginnings</h3>
               <p className="timeline__p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
@@ -109,6 +116,9 @@ const SecondPage = () => (
           </div>
           <div className="container2">
             <div className="timeline__content-2">
+              <div className="timeline__circle">
+                <img src={airplane} className="timeline__img" alt="" />
+              </div>
               <h4>March 2011</h4>
               <h3>An Agency is Born</h3>
               <p className="timeline__p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
@@ -116,6 +126,9 @@ const SecondPage = () => (
           </div>
           <div className="container3">
             <div className="timeline__content-3">
+              <div className="timeline__circle">
+                <img src={laptop} className="timeline__img" alt="" />
+              </div>
               <h4>December 2012</h4>
               <h3>Transition to Full Service</h3>
               <p className="timeline__p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
@@ -123,9 +136,25 @@ const SecondPage = () => (
           </div>
           <div className="container4">
             <div className="timeline__content-4">
+              <div className="timeline__circle">
+                <img src={mountain2} className="timeline__img" alt="" />
+              </div>
               <h4>July 2014</h4>
               <h3>Phase Two Expansion</h3>
               <p className="timeline__p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+            </div>
+          </div>
+          <div className="container5">
+            <div className="timeline__circle-last">
+              <h4 className="timeline__last">
+              Be Part
+                {' '}
+                <br />
+              Of Our
+                {' '}
+                <br />
+              Story!
+              </h4>
             </div>
           </div>
         </div>
