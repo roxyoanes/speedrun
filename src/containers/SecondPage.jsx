2c63bc9a62e93a83cda86793445f18d84ed56b25
roxioanes <roxioanes11@gmail.com>
2019-04-23 12:10:37 +0200

    contact card background and inputs

--- src/containers/SecondPage.jsx
+++ src/containers/SecondPage.jsx
@@ -237,15 +237,19 @@ const SecondPage = () => (
         <img src={creative} className="img" alt="" />
       </div>
       <div className="contact">
-        <h2 className="CONTACT__title">CONTACT</h2>
-        <h1 className="CONTACT__sub-title">Lorem ipsum dolor sit amet consectetur.</h1>
+        <h2 className="contact__title">CONTACT US</h2>
+        <h1 className="contact__sub-title">Lorem ipsum dolor sit amet consectetur.</h1>
         <div className="inputs">
-          <input className="inputs__name input" type="text" placeholder="Your Name*" />
-          <input className="inputs__email input" type="text" placeholder="Your Email*" />
-          <input className="inputs__phone input" type="text" placeholder="Your Phone*" />
-          <input className="inputs__message input" type="text" placeholder="Your Message*" />
+          <div className="inputs__left">
+            <input className="inputs__name input" type="text" placeholder="Your Name*" />
+            <input className="inputs__email input" type="text" placeholder="Your Email*" />
+            <input className="inputs__phone input" type="text" placeholder="Your Phone*" />
+          </div>
+          <textarea className="inputs__message input" type="text" placeholder="Your Message*" />
         </div>
-        <button className="button" type="button">SEND MESSAGE</button>
+        <div className="button-btn">
+          <button className="button" type="button">SEND MESSAGE</button>
+        </div>
       </div>
     </div>
   </div>
