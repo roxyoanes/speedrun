159375bf80353538a293e57ab81ef6c346bd6cb9
roxioanes <roxioanes11@gmail.com>
2019-04-23 16:36:52 +0200

    footer

--- src/containers/SecondPage.jsx
+++ src/containers/SecondPage.jsx
@@ -251,6 +251,27 @@ const SecondPage = () => (
           <button className="button" type="button">SEND MESSAGE</button>
         </div>
       </div>
+      <div className="footer">
+        <p className="footer__rights">Copyright © Your Website 2019</p>
+        <div className="footer__icons">
+          <span className="fa-stack fa-2x">
+            <i className="fas fa-circle fa-stack-2x" />
+            <i className="fab fa-twitter fa-stack-1x fa-inverse" />
+          </span>
+          <span className="fa-stack fa-2x">
+            <i className="fas fa-circle fa-stack-2x" />
+            <i className="fab fa-facebook-f fa-stack-1x fa-inverse" />
+          </span>
+          <span className="fa-stack fa-2x">
+            <i className="fas fa-circle fa-stack-2x" />
+            <i className="fab fa-linkedin-in fa-stack-1x fa-inverse" />
+          </span>
+        </div>
+        <div className="footer__right-side">
+          <a className="footer__right-side__a" href="#footer">Privacy Policy </a>
+          <a className="footer__right-side__a" href="#footer">Terms of Use</a>
+        </div>
+      </div>
     </div>
   </div>
 );
