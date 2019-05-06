c1cfac8c88256cc49ae14f038c02c4c025334e7a
roxioanes <roxioanes11@gmail.com>
2019-05-06 15:18:45 +0200

    menu tablet and mobile devices

--- src/containers/Components-secondpage/Navbar.jsx
+++ src/containers/Components-secondpage/Navbar.jsx
@@ -1,38 +1,39 @@
 import React from "react";
 
-import "../../styles/second-page.scss";
-import "../../styles/default.scss";
-
 const Navbar = ({ scrolled }) => (
   <div className={scrolled ? "scrolled" : "navbar"}>
     <a className="start" href="#top">
-              Start Bootstrap
+      Start Bootstrap
     </a>
     <div className="navbar__right-side">
+      <button type="button" className="button button--menu tablet">
+        MENU
+        <i className="fas fa-bars" />
+      </button>
       <nav className="navbar__elements">
         <a href="#service">
           <button className="navbar__navbar-button" type="button">
-                  SERVICES
+            SERVICES
           </button>
         </a>
         <a href="#portfolio">
           <button className="navbar__navbar-button" type="button">
-                  PORTFOLIO
+            PORTFOLIO
           </button>
         </a>
         <a href="#about">
           <button className="navbar__navbar-button" type="button">
-                  ABOUT
+            ABOUT
           </button>
         </a>
         <a href="#team">
           <button className="navbar__navbar-button" type="button">
-                  TEAM
+            TEAM
           </button>
         </a>
         <a href="#contact">
           <button className="navbar__navbar-button" type="button">
-                  CONTACT
+            CONTACT
           </button>
         </a>
       </nav>
