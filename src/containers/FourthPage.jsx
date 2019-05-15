f9f9a0d4cf93f0c492f1b06ed7ad5971ad7c18e9
roxioanes <roxioanes11@gmail.com>
2019-05-15 17:29:58 +0200

    card6

--- src/containers/FourthPage.jsx
+++ src/containers/FourthPage.jsx
@@ -165,6 +165,17 @@ const FourthPage = () => (
           </div>
         </div>
       </div>
+      <div className="card-6">
+        <div className="card-6__container">
+          <h2 className="card-6__subheading">The buttons below are impossible to resist...</h2>
+          <a href="#top" className="button-card button-card--white">
+            Click Me!
+          </a>
+          <a href="#top" className="button-card button-card--color">
+            Look At Me!
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 );
