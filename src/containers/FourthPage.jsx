826f6931ee87a2b5dc51531eadc14e5d1309aefe
roxioanes <roxioanes11@gmail.com>
2019-05-16 15:56:00 +0200

    footer

--- src/containers/FourthPage.jsx
+++ src/containers/FourthPage.jsx
@@ -179,8 +179,37 @@ const FourthPage = () => (
         </div>
       </div>
       <div className="card-7">
-        <div><iframe title="map" width="100%" height="250" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=37.7767817,-122.4167923&amp;q=1355%20Market%20St%20%23900%2C%20San%20Francisco%2C%20CA%2094103%2C%20USA+(map)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" /></div>
+        <div>
+          <iframe
+            title="map"
+            width="100%"
+            height="250"
+            src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=37.7767817,-122.4167923&amp;q=1355%20Market%20St%20%23900%2C%20San%20Francisco%2C%20CA%2094103%2C%20USA+(map)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
+            frameBorder="0"
+            scrolling="no"
+            marginHeight="0"
+            marginWidth="0"
+          />
+        </div>
       </div>
+      <div className="footer">
+        <div className="footer-icons">
+          <span className="fa-stack footer-icons fa-2x">
+            <i className="fas fa-circle fa-stack-2x" />
+            <i className="fab fa-facebook-f fa-stack-1x fa-inverse" />
+          </span>
+          <span className="fa-stack footer-icons fa-2x">
+            <i className="fas fa-circle fa-stack-2x" />
+            <i className="fab fa-twitter fa-stack-1x fa-inverse" />
+          </span>
+          <span className="fa-stack footer-icons fa-2x">
+            <i className="fas fa-circle fa-stack-2x" />
+            <i className="fab fa-github fa-stack-1x fa-inverse" />
+          </span>
+        </div>
+        <p className="footer__p">Copyright © Your Website 2019</p>
+      </div>
+      
     </div>
   </div>
 );
