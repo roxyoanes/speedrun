4eeefb74fac9eb3411c57262922ba69892155fcc
roxioanes <roxioanes11@gmail.com>
2019-05-17 17:16:24 +0200

    toggle button menu

--- src/containers/Components-fourthpage/Navbar.jsx
+++ src/containers/Components-fourthpage/Navbar.jsx
@@ -7,6 +7,14 @@ class Navbar extends Component {
     showMenu: false,
   };
 
+  componentDidMount() {
+    document.querySelector("html").classList.add("smooth-scroll");
+  }
+
+  componentWillUnmount() {
+    document.querySelector("html").classList.remove("smooth-scroll");
+  }
+
   showMenu = (event) => {
     event.preventDefault();
     this.setState({ showMenu: true }, () => {
@@ -25,7 +33,12 @@ class Navbar extends Component {
     return (
       <div className="navbar">
         <button onClick={this.showMenu} type="button" className="button-menu">
-          <i className="fas fa-bars" />
+          {showMenu ? (
+            <i className="fas fa-times" />
+          ) : (
+            <i className="fas fa-bars" />
+          )
+          }
         </button>
         {showMenu ? (
           <nav className="menu-drop" id="active">
