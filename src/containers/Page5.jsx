629fae616ff18372ead6d6fce411bc042999560a
roxioanes <roxioanes11@gmail.com>
2019-05-23 18:06:56 +0200

    About and Services page

--- src/containers/Page5.jsx
+++ src/containers/Page5.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "../styles/page5.scss";
 import logonavbar5 from "./images/logonavbar5.png";
 import landing5 from "./images/landing5.png";
+import aboutimg from "./images/aboutimg.jpg";
 
 const Page5 = () => (
   <div className="page5">
@@ -44,17 +45,94 @@ const Page5 = () => (
             {" "}
             <strong className="landing__bold">BEST</strong>
             {" "}
-            One Page Web Template
+            One Page Web
+            Template
           </h2>
           <p className="landing__paragraph">
             Lorem Ipsum is simply dummy text of the printing and type setting
             industry. Lorem Ipsum has been the galley of type and scrambled it
             to make a type specimen.
           </p>
-          <button className="button" type="button">READ MORE</button>
+          <button className="button" type="button">
+            READ MORE
+          </button>
         </div>
         <div className="landing__img-wrapper">
           <img src={landing5} className="landing__img" alt="landing-img" />
+        </div>
+      </div>
+      <div className="about">
+        <div className="about__part1">
+          <h2 className="about__heading">ABOUT US</h2>
+          <img src={aboutimg} className="about__img" alt="img" />
+        </div>
+        <div className="about__part2">
+          <h3 className="about__subheading">Lorem Ipsum has been the industry's standard dummy text ever..</h3>
+          <p className="about__paragraph">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. PageMaker including versions of Lorem Ipsum.
+          </p>
+          <p className="about__paragraph">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+      </div>
+      <div className="services">
+        <h2 className="services__heading">SERVICES</h2>
+        <div className="services__all-cards">
+          <div className="services__card">
+            <div className="services__card-dashed-area">
+              <span className="services__card-icon">
+                <i className="fab fa-android icon" />
+              </span>
+              <h3 className="services__card-title">ANDROID</h3>
+              <p className="services__card-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+            </div>
+          </div>
+          <div className="services__card">
+            <div className="services__card-dashed-area">
+              <span className="services__card-icon">
+                <i className="fab fa-apple icon" />
+              </span>
+              <h3 className="services__card-title">APPLE IOS</h3>
+              <p className="services__card-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+            </div>
+          </div>
+          <div className="services__card">
+            <div className="services__card-dashed-area">
+              <span className="services__card-icon">
+                <i className="fab fa-html5 icon" />
+              </span>
+              <h3 className="services__card-title">DESIGN</h3>
+              <p className="services__card-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+            </div>
+          </div>
+          <div className="services__card">
+            <div className="services__card-dashed-area">
+              <span className="services__card-icon">
+                <i className="fab fa-dropbox icon" />
+              </span>
+              <h3 className="services__card-title">CONCEPT</h3>
+              <p className="services__card-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+            </div>
+          </div>
+          <div className="services__card">
+            <div className="services__card-dashed-area">
+              <span className="services__card-icon">
+                <i className="fab fa-slack icon" />
+              </span>
+              <h3 className="services__card-title">USER RESEARCH</h3>
+              <p className="services__card-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+            </div>
+          </div>
+          <div className="services__card">
+            <div className="services__card-dashed-area">
+              <span className="services__card-icon">
+                <i className="fas fa-users icon" />
+              </span>
+              <h3 className="services__card-title">USER EXPERIENCE</h3>
+              <p className="services__card-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
