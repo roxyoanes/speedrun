f442e70f8d086a8250f81b14c70e4c63cd9c30e2
roxioanes <roxioanes11@gmail.com>
2019-05-24 12:37:58 +0200

    Princing styling

--- src/containers/Page5.jsx
+++ src/containers/Page5.jsx
@@ -185,6 +185,74 @@ const Page5 = () => (
           </div>
         </div>
       </div>
+      <div className="pricing">
+        <h2 className="pricing__heading">PRICING</h2>
+        <p className="pricing__description">Lorem ipsum dolor sit amet, consectetur adipiscing.</p>
+        <div className="pricing__cards-container">
+          <div className="pricing__card">
+            <h2 className="pricing__title">Starter</h2>
+            <div className="pricing__content">
+              <p className="pricing__price">
+                <sup>$</sup>
+                8.00
+              </p>
+              <p>Best Template</p>
+              <p>Clean and modern</p>
+              <p>Bootstrap & HTML5</p>
+              <p>24/7 support</p>
+            </div>
+            <div className="pricing__button-wrapper">
+              <button type="button" className="pricing-button">Buy Now</button>
+            </div>
+          </div>
+          <div className="pricing__card">
+            <h2 className="pricing__title">Popular</h2>
+            <div className="pricing__content">
+              <p className="pricing__price">
+                <sup>$</sup>
+                12.00
+              </p>
+              <p>Best Template</p>
+              <p>Clean and modern</p>
+              <p>Bootstrap & HTML5</p>
+              <p>24/7 support</p>
+            </div>
+            <div className="pricing__button-wrapper">
+              <button type="button" className="pricing-button">Buy Now</button>
+            </div>
+          </div>
+          <div className="pricing__card">
+            <h2 className="pricing__title">Business</h2>
+            <div className="pricing__content">
+              <p className="pricing__price">
+                <sup>$</sup>
+                16.00
+              </p>
+              <p>Best Template</p>
+              <p>Clean and modern</p>
+              <p>Bootstrap & HTML5</p>
+              <p>24/7 support</p>
+            </div>
+            <div className="pricing__button-wrapper">
+              <button type="button" className="pricing-button">Buy Now</button>
+            </div>
+          </div>
+          <div className="pricing__card--noshow">
+            <h2 className="pricing__title">Enterprise</h2>
+            <div className="pricing__content">
+              <p className="pricing__price">
+                <sup>$</sup>
+                20.00
+              </p>
+              <p>Best Template</p>
+              <p>Clean and modern</p>
+              <p>Bootstrap & HTML5</p>
+              <p>24/7 support</p>
+            </div>
+            <button type="button" className="pricing-button">Buy Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );
