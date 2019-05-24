e2b4d742d494f227084fc252b602c6f21b752947
roxioanes <roxioanes11@gmail.com>
2019-05-24 15:38:44 +0200

    Team card styling

--- src/containers/Page5.jsx
+++ src/containers/Page5.jsx
@@ -10,6 +10,9 @@ import gallery3 from "./images/gallery3.jpg";
 import gallery4 from "./images/gallery4.jpg";
 import gallery5 from "./images/gallery5.jpg";
 import gallery6 from "./images/gallery6.jpg";
+import teamPic1 from "./images/teamPic1.jpg";
+import teamPic2 from "./images/teamPic2.jpg";
+import teamPic3 from "./images/teamPic3.jpg";
 
 const Page5 = () => (
   <div className="page5">
@@ -47,11 +50,7 @@ const Page5 = () => (
       <div className="landing">
         <div className="landing__text-section">
           <h2 className="landing__heading">
-            The
-            {" "}
-            <strong className="landing__bold">BEST</strong>
-            {" "}
-            One Page Web
+            The <strong className="landing__bold">BEST</strong> One Page Web
             Template
           </h2>
           <p className="landing__paragraph">
@@ -73,12 +72,24 @@ const Page5 = () => (
           <img src={aboutimg} className="about__img" alt="img" />
         </div>
         <div className="about__part2">
-          <h3 className="about__subheading">Lorem Ipsum has been the industry's standard dummy text ever..</h3>
+          <h3 className="about__subheading">
+            Lorem Ipsum has been the industry's standard dummy text ever..
+          </h3>
           <p className="about__paragraph">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. PageMaker including versions of Lorem Ipsum.
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. PageMaker including
+            versions of Lorem Ipsum.
           </p>
           <p className="about__paragraph">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged like Aldus PageMaker including versions of Lorem Ipsum.
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged like Aldus PageMaker including
+            versions of Lorem Ipsum.
           </p>
         </div>
       </div>
@@ -91,7 +102,11 @@ const Page5 = () => (
                 <i className="fab fa-android icon" />
               </span>
               <h3 className="services__card-title">ANDROID</h3>
-              <p className="services__card-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+              <p className="services__card-paragraph">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text.
+              </p>
             </div>
           </div>
           <div className="services__card">
@@ -100,7 +115,11 @@ const Page5 = () => (
                 <i className="fab fa-apple icon" />
               </span>
               <h3 className="services__card-title">APPLE IOS</h3>
-              <p className="services__card-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+              <p className="services__card-paragraph">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text.
+              </p>
             </div>
           </div>
           <div className="services__card">
@@ -109,7 +128,11 @@ const Page5 = () => (
                 <i className="fab fa-html5 icon" />
               </span>
               <h3 className="services__card-title">DESIGN</h3>
-              <p className="services__card-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+              <p className="services__card-paragraph">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text.
+              </p>
             </div>
           </div>
           <div className="services__card">
@@ -118,7 +141,11 @@ const Page5 = () => (
                 <i className="fab fa-dropbox icon" />
               </span>
               <h3 className="services__card-title">CONCEPT</h3>
-              <p className="services__card-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+              <p className="services__card-paragraph">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text.
+              </p>
             </div>
           </div>
           <div className="services__card">
@@ -127,7 +154,11 @@ const Page5 = () => (
                 <i className="fab fa-slack icon" />
               </span>
               <h3 className="services__card-title">USER RESEARCH</h3>
-              <p className="services__card-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+              <p className="services__card-paragraph">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text.
+              </p>
             </div>
           </div>
           <div className="services__card">
@@ -136,58 +167,90 @@ const Page5 = () => (
                 <i className="fas fa-users icon" />
               </span>
               <h3 className="services__card-title">USER EXPERIENCE</h3>
-              <p className="services__card-paragraph">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+              <p className="services__card-paragraph">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text.
+              </p>
             </div>
           </div>
         </div>
       </div>
       <div className="gallery">
         <h2 className="gallery__heading">GALLERY</h2>
-        <p className="gallery__description">Lorem ipsum dolor sit amet, consectetur adipiscing.</p>
+        <p className="gallery__description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing.
+        </p>
         <div className="gallery__tags-container">
-          <a href="#top" className="gallery__tag">ALL</a>
-          <a href="#top" className="gallery__tag gallery__backslash">ARTWORK</a>
-          <a href="#top" className="gallery__tag gallery__backslash">CREATIVE</a>
-          <a href="#top" className="gallery__tag gallery__backslash">NATURE</a>
-          <a href="#top" className="gallery__tag gallery__backslash">OUTSIDE</a>
-          <a href="#top" className="gallery__tag gallery__backslash">PHOTOGRAPHY</a>
+          <a href="#top" className="gallery__tag">
+            ALL
+          </a>
+          <a href="#top" className="gallery__tag gallery__backslash">
+            ARTWORK
+          </a>
+          <a href="#top" className="gallery__tag gallery__backslash">
+            CREATIVE
+          </a>
+          <a href="#top" className="gallery__tag gallery__backslash">
+            NATURE
+          </a>
+          <a href="#top" className="gallery__tag gallery__backslash">
+            OUTSIDE
+          </a>
+          <a href="#top" className="gallery__tag gallery__backslash">
+            PHOTOGRAPHY
+          </a>
         </div>
         <div className="gallery__images-container">
           <div className="gallery__card">
             <img src={gallery1} className="gallery__img" alt="hand" />
             <h5 className="gallery__card-title">1st gallery Item</h5>
-            <h6 className="gallery__card-subheading">Nullam id dolor id nibh ultricies vehicula.</h6>
+            <h6 className="gallery__card-subheading">
+              Nullam id dolor id nibh ultricies vehicula.
+            </h6>
           </div>
           <div className="gallery__card">
             <img src={gallery2} className="gallery__img" alt="hand" />
             <h5 className="gallery__card-title">2nd gallery Item</h5>
-            <h6 className="gallery__card-subheading">Nullam id dolor id nibh ultricies vehicula.</h6>
+            <h6 className="gallery__card-subheading">
+              Nullam id dolor id nibh ultricies vehicula.
+            </h6>
           </div>
           <div className="gallery__card">
             <img src={gallery3} className="gallery__img" alt="hand" />
             <h5 className="gallery__card-title">3rd gallery Item</h5>
-            <h6 className="gallery__card-subheading">Nullam id dolor id nibh ultricies vehicula.</h6>
+            <h6 className="gallery__card-subheading">
+              Nullam id dolor id nibh ultricies vehicula.
+            </h6>
           </div>
           <div className="gallery__card">
             <img src={gallery4} className="gallery__img" alt="hand" />
             <h5 className="gallery__card-title">4th gallery Item</h5>
-            <h6 className="gallery__card-subheading">Nullam id dolor id nibh ultricies vehicula.</h6>
+            <h6 className="gallery__card-subheading">
+              Nullam id dolor id nibh ultricies vehicula.
+            </h6>
           </div>
           <div className="gallery__card">
             <img src={gallery5} className="gallery__img" alt="hand" />
             <h5 className="gallery__card-title">5th gallery Item</h5>
-            <h6 className="gallery__card-subheading">Nullam id dolor id nibh ultricies vehicula.</h6>
+            <h6 className="gallery__card-subheading">
+              Nullam id dolor id nibh ultricies vehicula.
+            </h6>
           </div>
           <div className="gallery__card">
             <img src={gallery6} className="gallery__img" alt="hand" />
             <h5 className="gallery__card-title">6th gallery Item</h5>
-            <h6 className="gallery__card-subheading">Nullam id dolor id nibh ultricies vehicula.</h6>
+            <h6 className="gallery__card-subheading">
+              Nullam id dolor id nibh ultricies vehicula.
+            </h6>
           </div>
         </div>
       </div>
       <div className="pricing">
         <h2 className="pricing__heading">PRICING</h2>
-        <p className="pricing__description">Lorem ipsum dolor sit amet, consectetur adipiscing.</p>
+        <p className="pricing__description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing.
+        </p>
         <div className="pricing__cards-container">
           <div className="pricing__card">
             <h2 className="pricing__title">Starter</h2>
@@ -202,7 +265,9 @@ const Page5 = () => (
               <p>24/7 support</p>
             </div>
             <div className="pricing__button-wrapper">
-              <button type="button" className="pricing-button">Buy Now</button>
+              <button type="button" className="pricing-button">
+                Buy Now
+              </button>
             </div>
           </div>
           <div className="pricing__card">
@@ -218,7 +283,9 @@ const Page5 = () => (
               <p>24/7 support</p>
             </div>
             <div className="pricing__button-wrapper">
-              <button type="button" className="pricing-button">Buy Now</button>
+              <button type="button" className="pricing-button">
+                Buy Now
+              </button>
             </div>
           </div>
           <div className="pricing__card">
@@ -234,7 +301,9 @@ const Page5 = () => (
               <p>24/7 support</p>
             </div>
             <div className="pricing__button-wrapper">
-              <button type="button" className="pricing-button">Buy Now</button>
+              <button type="button" className="pricing-button">
+                Buy Now
+              </button>
             </div>
           </div>
           <div className="pricing__card--noshow">
@@ -249,7 +318,74 @@ const Page5 = () => (
               <p>Bootstrap & HTML5</p>
               <p>24/7 support</p>
             </div>
-            <button type="button" className="pricing-button">Buy Now</button>
+            <button type="button" className="pricing-button">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="team">
+        <h2 className="team__heading">TEAM</h2>
+        <p className="team__description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing.
+        </p>
+        <div className="team-cards-container">
+          <div className="team__card">
+            <div className="team__box">
+              <div className="team__box-shadow">
+                <span className="team__icons">
+                  <i className="fab fa-twitter team-icon" />
+                  <i className="fab fa-facebook team-icon" />
+                  <i className="fab fa-pinterest team-icon" />
+                  <i className="fab fa-google-plus team-icon" />
+                </span>
+              </div>
+              <img src={teamPic1} className="team__img" alt="person" />
+            </div>
+            <h3 className="team__person-name">Tom Rensed</h3>
+            <h6 className="team__position">Chief Executive Officer</h6>
+            <p className="team__description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing consectetur
+              elit proin consequat.
+            </p>
+          </div>
+          <div className="team__card">
+            <div className="team__box">
+              <div className="team__box-shadow">
+                <span className="team__icons">
+                  <i className="fab fa-twitter team-icon" />
+                  <i className="fab fa-facebook team-icon" />
+                  <i className="fab fa-pinterest team-icon" />
+                  <i className="fab fa-google-plus team-icon" />
+                </span>
+              </div>
+              <img src={teamPic2} className="team__img" alt="person" />
+            </div>
+            <h3 className="team__person-name">Kathren Mory</h3>
+            <h6 className="team__position">Vice President</h6>
+            <p className="team__description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing consectetur
+              elit proin consequat.
+            </p>
+          </div>
+          <div className="team__card">
+            <div className="team__box">
+              <div className="team__box-shadow">
+                <span className="team__icons">
+                  <i className="fab fa-twitter team-icon" />
+                  <i className="fab fa-facebook team-icon" />
+                  <i className="fab fa-pinterest team-icon" />
+                  <i className="fab fa-google-plus team-icon" />
+                </span>
+              </div>
+              <img src={teamPic3} className="team__img" alt="person" />
+            </div>
+            <h3 className="team__person-name">Lancer Jack</h3>
+            <h6 className="team__position">Senior Manager</h6>
+            <p className="team__description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing consectetur
+              elit proin consequat.
+            </p>
           </div>
         </div>
       </div>
