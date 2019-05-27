f75a65011abb11b50859856899477c52b12cfda5
roxioanes <roxioanes11@gmail.com>
2019-05-27 18:17:19 +0200

    Dropdown menu functionality

--- src/containers/Components-page5/Navbar.jsx
+++ src/containers/Components-page5/Navbar.jsx
@@ -1,38 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 
 import logonavbar5 from "../images/logonavbar5.png";
 
-const Navbar = () => (
-  <div className="navbar">
-    <a href="#top" className="navbar__logo">
-      <img src={logonavbar5} className="navbar__logo-img" alt="logo" />
-    </a>
-    <div className="navbar__right-side">
-      <button className="navbar-button" type="button">
-        <i className="fas fa-bars" />
-      </button>
-      <nav className="navbar__elements">
-        <a href="#top" className="navbar__list-item">
-          HOME
+class Navbar extends Component {
+  state = {
+    showMenu: false,
+  };
+
+  showMenu = (event) => {
+    event.preventDefault();
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener("click", this.closeMenu);
+    });
+  };
+
+  closeMenu = () => {
+    this.setState({ showMenu: false }, () => {
+      document.removeEventListener("click", this.closeMenu);
+    });
+  };
+
+  render() {
+    const { showMenu } = this.state;
+    return (
+      <div className="navbar">
+        <a href="#top" className="navbar__logo">
+          <img src={logonavbar5} className="navbar__logo-img" alt="logo" />
         </a>
-        <a href="#top" className="navbar__list-item">
-          ABOUT US
-        </a>
-        <a href="#top" className="navbar__list-item">
-          SERVICES
-        </a>
-        <a href="#top" className="navbar__list-item">
-          PORTFOLIO
-        </a>
-        <a href="#top" className="navbar__list-item">
-          TEAM
-        </a>
-        <a href="#top" className="navbar__list-item">
-          CONTACT
-        </a>
-      </nav>
-    </div>
-  </div>
-);
+        <div className="navbar__right-side">
+          <button
+            onClick={this.showMenu}
+            className="navbar-button"
+            type="button"
+          >
+            <i className="fas fa-bars" />
+          </button>
+        </div>
+        {showMenu ? (
+          <nav className="dropdown">
+            {[
+              { value: "HOME", href: "#top" },
+              { value: "ABOUT US", href: "#top" },
+              { value: "SERVICES", href: "#top" },
+              { value: "PORTFOLIO", href: "#top" },
+              { value: "TEAM", href: "#top" },
+              { value: "CONTACT", href: "#top" }
+            ].map(navElement => (
+              <a href={navElement.href} className="navbar__list-item">
+                {navElement.value}
+              </a>
+            ))}
+          </nav>
+        ) : null}
+      </div>
+    );
+  }
+}
 
 export default Navbar;
