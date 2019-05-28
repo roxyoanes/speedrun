de05fcb04d87cf018e2720abc6b0d71e9433e985
roxioanes <roxioanes11@gmail.com>
2019-05-28 18:03:52 +0200

    About media queries

--- src/containers/Components-page5/About.jsx
+++ src/containers/Components-page5/About.jsx
@@ -4,8 +4,8 @@ import aboutimg from "../images/aboutimg.jpg";
 
 const About = () => (
   <div className="about" id="about">
-    <div className="about__part1">
-      <h2 className="about__heading">ABOUT US</h2>
+    <h2 className="about__heading">ABOUT US</h2>
+    <div className="about__wrapper">
       <img
         src={aboutimg}
         className="about__img"
@@ -16,32 +16,32 @@ const About = () => (
         data-aos-offset="0"
         data-aos-duration="1500"
       />
-    </div>
-    <div
-      className="about__part2"
-      data-aos="fade-down"
-      data-aos-easing="linear"
-      data-aos-duration="1500"
-    >
-      <h3 className="about__subheading">
-        Lorem Ipsum has been the industry's standard dummy text ever..
-      </h3>
-      <p className="about__paragraph">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. PageMaker including versions
-        of Lorem Ipsum.
-      </p>
-      <p className="about__paragraph">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged like Aldus PageMaker including versions of Lorem
-        Ipsum.
-      </p>
+      <div
+        className="about__text"
+        data-aos="fade-down"
+        data-aos-easing="linear"
+        data-aos-duration="1500"
+      >
+        <h3 className="about__subheading">
+          Lorem Ipsum has been the industry's standard dummy text ever..
+        </h3>
+        <p className="about__paragraph">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. PageMaker including
+          versions of Lorem Ipsum.
+        </p>
+        <p className="about__paragraph">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged like Aldus PageMaker including
+          versions of Lorem Ipsum.
+        </p>
+      </div>
     </div>
   </div>
 );
