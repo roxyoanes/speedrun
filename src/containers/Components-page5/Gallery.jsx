34bc16df1e8d75cc1153ca96c23f52bc4bcfb55d
roxioanes <roxioanes11@gmail.com>
2019-05-31 09:11:54 +0200

    Add icons on top of image

--- src/containers/Components-page5/Gallery.jsx
+++ src/containers/Components-page5/Gallery.jsx
@@ -28,42 +28,120 @@ const Gallery = () => (
     </div>
     <div className="gallery__images-container">
       <div className="gallery__card">
-        <div className="gallery__img1 gallery__image-style" />
+        <div className="gallery__img1 gallery__image-style">
+          <span className="fa-stack fa-2x container-icon1">
+            <a href="#top" className="gallery__icon-eye">
+              <i className="fas fa-square fa-stack-2x" />
+              <i className="fa fa-eye fa-stack-1x fa-inverse" />
+            </a>
+          </span>
+          <span className="fa-stack fa-2x container-icon2">
+            <a href="#top" className="gallery__icon-link">
+              <i className="fas fa-square fa-stack-2x" />
+              <i className="fa fa-link fa-stack-1x fa-inverse" />
+            </a>
+          </span>
+        </div>
         <h5 className="gallery__card-title">1st gallery Item</h5>
         <h6 className="gallery__card-subheading">
           Nullam id dolor id nibh ultricies vehicula.
         </h6>
       </div>
       <div className="gallery__card">
-        <div className="gallery__img2 gallery__image-style" />
+        <div className="gallery__img2 gallery__image-style">
+          <span className="fa-stack fa-2x container-icon1">
+            <a href="#top" className="gallery__icon-eye">
+              <i className="fas fa-square fa-stack-2x" />
+              <i className="fa fa-eye fa-stack-1x fa-inverse" />
+            </a>
+          </span>
+          <span className="fa-stack fa-2x container-icon2">
+            <a href="#top" className="gallery__icon-link">
+              <i className="fas fa-square fa-stack-2x" />
+              <i className="fa fa-link fa-stack-1x fa-inverse" />
+            </a>
+          </span>
+        </div>
         <h5 className="gallery__card-title">2nd gallery Item</h5>
         <h6 className="gallery__card-subheading">
           Nullam id dolor id nibh ultricies vehicula.
         </h6>
       </div>
       <div className="gallery__card">
-        <div className="gallery__img3 gallery__image-style" />
+        <div className="gallery__img3 gallery__image-style">
+          <span className="fa-stack fa-2x container-icon1">
+            <a href="#top" className="gallery__icon-eye">
+              <i className="fas fa-square fa-stack-2x" />
+              <i className="fa fa-eye fa-stack-1x fa-inverse" />
+            </a>
+          </span>
+          <span className="fa-stack fa-2x container-icon2">
+            <a href="#top" className="gallery__icon-link">
+              <i className="fas fa-square fa-stack-2x" />
+              <i className="fa fa-link fa-stack-1x fa-inverse" />
+            </a>
+          </span>
+        </div>
         <h5 className="gallery__card-title">3rd gallery Item</h5>
         <h6 className="gallery__card-subheading">
           Nullam id dolor id nibh ultricies vehicula.
         </h6>
       </div>
       <div className="gallery__card">
-        <div className="gallery__img4 gallery__image-style" />
+        <div className="gallery__img4 gallery__image-style">
+          <span className="fa-stack fa-2x container-icon1">
+            <a href="#top" className="gallery__icon-eye">
+              <i className="fas fa-square fa-stack-2x" />
+              <i className="fa fa-eye fa-stack-1x fa-inverse" />
+            </a>
+          </span>
+          <span className="fa-stack fa-2x container-icon2">
+            <a href="#top" className="gallery__icon-link">
+              <i className="fas fa-square fa-stack-2x" />
+              <i className="fa fa-link fa-stack-1x fa-inverse" />
+            </a>
+          </span>
+        </div>
         <h5 className="gallery__card-title">4th gallery Item</h5>
         <h6 className="gallery__card-subheading">
           Nullam id dolor id nibh ultricies vehicula.
         </h6>
       </div>
       <div className="gallery__card">
-        <div className="gallery__img5 gallery__image-style" />
+        <div className="gallery__img5 gallery__image-style">
+          <span className="fa-stack fa-2x container-icon1">
+            <a href="#top" className="gallery__icon-eye">
+              <i className="fas fa-square fa-stack-2x" />
+              <i className="fa fa-eye fa-stack-1x fa-inverse" />
+            </a>
+          </span>
+          <span className="fa-stack fa-2x container-icon2">
+            <a href="#top" className="gallery__icon-link">
+              <i className="fas fa-square fa-stack-2x" />
+              <i className="fa fa-link fa-stack-1x fa-inverse" />
+            </a>
+          </span>
+        </div>
         <h5 className="gallery__card-title">5th gallery Item</h5>
         <h6 className="gallery__card-subheading">
           Nullam id dolor id nibh ultricies vehicula.
         </h6>
       </div>
       <div className="gallery__card">
-        <div className="gallery__img6 gallery__image-style" />
+        <div className="gallery__img6 gallery__image-style">
+          <span className="fa-stack fa-2x container-icon1">
+            <a href="#top" className="gallery__icon-eye">
+              <i className="fas fa-square fa-stack-2x" />
+              <i className="fa fa-eye fa-stack-1x fa-inverse" />
+            </a>
+          </span>
+          <span className="fa-stack fa-2x container-icon2">
+            <a href="#top" className="gallery__icon-link">
+              <i className="fas fa-square fa-stack-2x" />
+              <i className="fa fa-link fa-stack-1x fa-inverse" />
+            </a>
+          </span>
+        </div>
         <h5 className="gallery__card-title">6th gallery Item</h5>
         <h6 className="gallery__card-subheading">
           Nullam id dolor id nibh ultricies vehicula.
