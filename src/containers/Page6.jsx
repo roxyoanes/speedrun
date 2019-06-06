25a1d8d650323d7ac1a9e2b69f7826335c9da576
roxioanes <roxioanes11@gmail.com>
2019-06-06 15:51:34 +0200

    Style footer and subfooter

--- src/containers/Page6.jsx
+++ src/containers/Page6.jsx
@@ -122,6 +122,61 @@ class Page6 extends Component {
           <Route path={`${match.path}/portfolio`} component={Portfolio} />
           <Route path={`${match.path}/pricing`} component={Pricing} />
           <Route path={`${match.path}/contact`} component={Contact} />
+          <div className="subfooter">
+            <div className="subfooter__contact">
+              <h5 className="subfooter__title">Our Contact</h5>
+              <strong>Bootstrap company Inc.</strong>
+              <br />
+              JC Main Road, Near Silnile tower
+              <br />
+              Pin-21542 NewYork US.
+              <div className="subfooter__details">
+                <p className="subfooter__p">(123) 456-789 - 1255-12584 </p>
+                <p className="subfooter__p">email@domainname.com</p>
+              </div>
+            </div>
+            <div className="subfooter__links">
+              <h5 className="subfooter__title">Quick Links</h5>
+              <div className="subfooter__list">
+                <a href="#top" className="subfooter__item">Latest Events</a>
+                <a href="#top" className="subfooter__item">Terms and Conditions</a>
+                <a href="#top" className="subfooter__item">Privacy Policy</a>
+                <a href="#top" className="subfooter__item">Career</a>
+                <a href="#top" className="subfooter__item">Contact us</a>
+              </div>
+            </div>
+            <div className="subfooter__posts">
+              <h5 className="subfooter__title">Latest Posts</h5>
+              <div className="subfooter__list">
+                <a href="#top" className="subfooter__item">Lorem ipsum dolor met consectetur adipisicing. Aorem psum dolor met consectetur adipisicing sit amet, consectetur adipisicing elit, of them jean shorts sed magna aliqua. Lorem ipsum dolor met.</a>
+                <a href="#top" className="subfooter__item">Pellentesque et pulvinar enim. Quisque at tempor ligula</a>
+                <a href="#top" className="subfooter__item">Natus error sit voluptatem accusantium doloremque</a>
+              </div>
+            </div>
+            <div className="subfooter__news">
+              <h5 className="subfooter__title">Recent News</h5>
+              <div className="subfooter__list">
+                <a href="#top" className="subfooter__item">Lorem ipsum dolor met consectetur adipisicing. </a>
+                <a href="#top" className="subfooter__item">Pellentesque et pulvinar enim. Quisque at tempor ligula</a>
+                <a href="#top" className="subfooter__item">Natus error sit voluptatem accusantium doloremque</a>
+              </div>
+            </div>
+          </div>
+          <div className="footer">
+            <div className="footer__copyright">
+              <p>
+                <span>© Bootstrap Template 2017 All right reserved. Template By</span>
+                <a href="http://webthemez.com/free-bootstrap-templates/" className="footer__website">WebThemez</a>
+              </p>
+            </div>
+            <span className="footer__icons-list">
+              <i className="fab fa-twitter footer-icon" />
+              <i className="fab fa-facebook footer-icon" />
+              <i className="fab fa-linkedin footer-icon" />
+              <i className="fab fa-pinterest footer-icon" />
+              <i className="fab fa-google-plus footer-icon" />
+            </span>
+          </div>
         </div>
       </div>
     );
