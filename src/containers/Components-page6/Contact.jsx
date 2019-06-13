737ca436c89d0f9bd4018fff92c9522664e8f7db
roxioanes <roxioanes11@gmail.com>
2019-06-13 16:58:17 +0200

    Style contact section

--- src/containers/Components-page6/Contact.jsx
+++ src/containers/Components-page6/Contact.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Contact = () => (
-  <h1>contact</h1>
+  <div className="contact">
+    <div className="contact__page-title">
+      <h2 className="contact__title">CONTACT US</h2>
+    </div>
+    <div className="contact__about-wrapper">
+      <h3 className="contact__text">
+        Get
+        {" "}
+        <span className="contact__text-color">in Touch</span>
+      </h3>
+      <p className="contact__p">Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas</p>
+      <p className="contact__p">Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas</p>
+    </div>
+    <form className="contact__info-wrapper">
+      <div className="contact__info-card">
+        <input type="text"  className="contact__form" id="name" />
+        <label HTMLfor="name" className="contact__label">Name</label>
+      </div>
+      <div className="contact__info-card">
+        <input type="text" className="contact__form" id="name" />
+        <label HTMLfor="name" className="contact__label">Email</label>
+      </div>
+      <div className="contact__info-card">
+        <textarea type="text" className="contact__form contact__textarea" id="name" />
+        <label HTMLfor="name" className="contact__label">Message</label>
+      </div>
+    </form>
+    <div className="contact__btn-wrapper">
+      <button type="button" className="button-contact">SEND</button>
+    </div>
+  </div>
 );
 
 export default Contact;
--- src/containers/Page6.jsx
+++ src/containers/Page6.jsx
@@ -11,6 +11,7 @@ import {
 } from "./Components-page6";
 
 import "../styles/page6.scss";
+import "../styles/page6-contact.scss";
 import { Page6navbar1, Subfooter, Footer } from "./Sections-page6";
 import logo6 from "./images/logo6.png";
 
