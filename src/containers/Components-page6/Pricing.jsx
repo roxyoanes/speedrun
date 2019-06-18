175a8c49df459f51e56adf462e6b59dfeb2e87fb
roxioanes <roxioanes11@gmail.com>
2019-06-18 10:43:22 +0200

    Page pricing add style

--- src/containers/Components-page6/Pricing.jsx
+++ src/containers/Components-page6/Pricing.jsx
@@ -1,7 +1,104 @@
 import React from "react";
 
 const Pricing = () => (
-  <h1>pricing</h1>
+  <div className="pricing">
+    <div className="pricing__page-title">
+      <h2 className="pricing__title">PRICING</h2>
+    </div>
+    <div className="pricing__about-wrapper">
+      <h3 className="pricing__text">
+        Our Best
+        {" "}
+        <span className="pricing__text-color">Plans</span>
+      </h3>
+      <p className="pricing__p">
+        Sed ut perspiciaatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo. Nemo enim ipsam voluptatem quia voluptas
+      </p>
+      <p className="pricing__p">
+        Sed ut perspiciaatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo. Nemo enim ipsam voluptatem quia voluptas
+      </p>
+    </div>
+    <div className="pricing__container">
+      <div className="pricing__card">
+        <div className="pricing__heading">
+          <h3>Basic</h3>
+        </div>
+        <div className="pricing__terms">
+          <h3 className="pricing__price">$15.00/Year</h3>
+        </div>
+        <div className="pricing__content">
+          <h3 className="pricing__properties">Responsive Design</h3>
+          <h3 className="pricing__properties">Bootstrap Design</h3>
+          <h3 className="pricing__properties">Unlimited Design</h3>
+          <h3 className="pricing__properties">Trial Version</h3>
+          <h3 className="pricing__properties">HTML5 CSS3 jQuery</h3>
+        </div>
+        <div className="pricing__action">
+          <button type="button" className="price-btn">GET NOW</button>
+        </div>
+      </div>
+      <div className="pricing__card">
+        <div className="pricing__heading">
+          <h3>Standard</h3>
+        </div>
+        <div className="pricing__terms">
+          <h3 className="pricing__price">$20.00/Year</h3>
+        </div>
+        <div className="pricing__content">
+          <h3 className="pricing__properties">Responsive Design</h3>
+          <h3 className="pricing__properties">Bootstrap Design</h3>
+          <h3 className="pricing__properties">Unlimited Design</h3>
+          <h3 className="pricing__properties">Trial Version</h3>
+          <h3 className="pricing__properties">HTML5 CSS3 jQuery</h3>
+        </div>
+        <div className="pricing__action">
+          <button type="button" className="price-btn">GET NOW</button>
+        </div>
+      </div>
+      <div className="pricing__card">
+        <div className="pricing__heading">
+          <h3>Advanced</h3>
+        </div>
+        <div className="pricing__terms">
+          <h3 className="pricing__price">$20.00/Year</h3>
+        </div>
+        <div className="pricing__content">
+          <h3 className="pricing__properties">Responsive Design</h3>
+          <h3 className="pricing__properties">Bootstrap Design</h3>
+          <h3 className="pricing__properties">Unlimited Design</h3>
+          <h3 className="pricing__properties">Trial Version</h3>
+          <h3 className="pricing__properties">HTML5 CSS3 jQuery</h3>
+        </div>
+        <div className="pricing__action">
+          <button type="button" className="price-btn">GET NOW</button>
+        </div>
+      </div>
+      <div className="pricing__card">
+        <div className="pricing__heading">
+          <h3>Mighty</h3>
+        </div>
+        <div className="pricing__terms">
+          <h3 className="pricing__price">$20.00/Year</h3>
+        </div>
+        <div className="pricing__content">
+          <h3 className="pricing__properties">Responsive Design</h3>
+          <h3 className="pricing__properties">Bootstrap Design</h3>
+          <h3 className="pricing__properties">Unlimited Design</h3>
+          <h3 className="pricing__properties">Trial Version</h3>
+          <h3 className="pricing__properties">HTML5 CSS3 jQuery</h3>
+        </div>
+        <div className="pricing__action">
+          <button type="button" className="price-btn">GET NOW</button>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default Pricing;
