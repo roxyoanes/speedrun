ab8d3bf1273e27352761c8463e2e68433e615099
roxioanes <roxioanes11@gmail.com>
2019-06-21 16:28:52 +0200

    Add div for grid

--- src/containers/Components-page6/About.jsx
+++ src/containers/Components-page6/About.jsx
@@ -12,101 +12,103 @@ const About = () => (
     <div className="about__page-title">
       <h2 className="about__title">ABOUT US</h2>
     </div>
-    <div className="about__img-wrapper">
-      <img src={pic} alt="man" className="about__img" />
-    </div>
-    <div className="about__about-wrapper">
-      <h3 className="about__text">
-        Our
-        {" "}
-        <span className="about__text-color">Company</span>
-      </h3>
-      <p className="about__p">
-        Sed ut perspiciaatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas
-      </p>
-      <p className="about__p">
-        Sed ut perspiciaatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas
-      </p>
-    </div>
-    <div className="about__reason-wrapper">
-      <h3 className="about__text--font">Why Choose Us?</h3>
-      <p className="about__p">
-        Sed ut perspiciaatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur.
-      </p>
-      <p className="about__p">
-        Sed ut perspiciaatis iste natus error sit voluptatem probably haven't
-        heard of them accusamus.
-      </p>
-    </div>
-    <div className="about__solution">
-      <h3 className="about__text--font">Our Solution</h3>
-    </div>
     <div className="about__wrapper">
-      <div className="about__accordion-card">
-        <Accordion
-          text="Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas"
-          btnText="First"
-          icon="fas fa-arrow-right"
-        />
-      </div>
-      <div className="about__accordion-card">
-        <Accordion
-          text="Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas"
-          btnText="Second"
-          icon="fas fa-arrow-right"
-        />
-      </div>
-      <div className="about__accordion-card">
-        <Accordion
-          text="Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas"
-          btnText="Third"
-          icon="fas fa-arrow-right"
-        />
-      </div>
-      <div className="about__accordion-card">
-        <Accordion
-          text="Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas"
-          btnText="Fourth"
-          icon="fas fa-arrow-right"
-        />
+      <img src={pic} alt="man" className="about__img" />
+      <div className="about__about-wrapper">
+        <h3 className="about__text">
+          Our
+          {" "}
+          <span className="about__text-color">Company</span>
+        </h3>
+        <p className="about__p">
+          Sed ut perspiciaatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+        </p>
+        <p className="about__p">
+          Sed ut perspiciaatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
+        </p>
       </div>
     </div>
-    <div className="about__expertise-wrapper">
-      <h3 className="about__text--font">Our Expertise</h3>
-      <div className="about__statistics-container">
-        <h6 className="about__statistics-text">Web Development</h6>
-        <div className="about__progress-container">
-          <div className="about__progress-bar" />
+    <div className="about__wrapper-part2">
+      <div className="about__reason-wrapper">
+        <h3 className="about__text--font">Why Choose Us?</h3>
+        <p className="about__p">
+          Sed ut perspiciaatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur.
+        </p>
+        <p className="about__p">
+          Sed ut perspiciaatis iste natus error sit voluptatem probably haven't
+          heard of them accusamus.
+        </p>
+      </div>
+      <div className="about__acc-wrapper">
+        <h3 className="about__text--font">Our Solution</h3>
+        <div className="about__accordion-card">
+          <Accordion
+            text="Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas"
+            btnText="First"
+            icon="fas fa-arrow-right"
+          />
+        </div>
+        <div className="about__accordion-card">
+          <Accordion
+            text="Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas"
+            btnText="Second"
+            icon="fas fa-arrow-right"
+          />
+        </div>
+        <div className="about__accordion-card">
+          <Accordion
+            text="Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas"
+            btnText="Third"
+            icon="fas fa-arrow-right"
+          />
+        </div>
+        <div className="about__accordion-card">
+          <Accordion
+            text="Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas"
+            btnText="Fourth"
+            icon="fas fa-arrow-right"
+          />
         </div>
       </div>
-      <div className="about__statistics-container">
-        <h6 className="about__statistics-text">Designing</h6>
-        <div className="about__progress-container">
-          <div className="about__progress-bar--color2" />
+      <div className="about__expertise-wrapper">
+        <h3 className="about__text--font">Our Expertise</h3>
+        <div className="about__statistics-container">
+          <h6 className="about__statistics-text">Web Development</h6>
+          <div className="about__progress-container">
+            <div className="about__progress-bar" />
+          </div>
         </div>
-      </div>
-      <div className="about__statistics-container">
-        <h6 className="about__statistics-text">User Experience</h6>
-        <div className="about__progress-container">
-          <div className="about__progress-bar--color3" />
+        <div className="about__statistics-container">
+          <h6 className="about__statistics-text">Designing</h6>
+          <div className="about__progress-container">
+            <div className="about__progress-bar--color2" />
+          </div>
         </div>
-      </div>
-      <div className="about__statistics-container">
-        <h6 className="about__statistics-text">Development</h6>
-        <div className="about__progress-container">
-          <div className="about__progress-bar--color4" />
+        <div className="about__statistics-container">
+          <h6 className="about__statistics-text">User Experience</h6>
+          <div className="about__progress-container">
+            <div className="about__progress-bar--color3" />
+          </div>
+        </div>
+        <div className="about__statistics-container">
+          <h6 className="about__statistics-text">Development</h6>
+          <div className="about__progress-container">
+            <div className="about__progress-bar--color4" />
+          </div>
         </div>
       </div>
     </div>
+
     <div className="about__team-container">
       <h3 className="about__text--font">Our Team</h3>
       <div className="about__members">
--- src/containers/Components-page6/Components-first/Landing.jsx
+++ src/containers/Components-page6/Components-first/Landing.jsx
@@ -6,46 +6,48 @@ import image2 from "../../images/landing2.jpg";
 const images = [image1, image2];
 
 class Landing extends Component {
-    state = {
-      currentImage: 0,
-    }
+  state = {
+    currentImage: 0,
+  };
 
-    componentDidMount() {
-      const { currentImage } = this.state;
-      const intervalId = setInterval(this.timer, 5000);
-      setInterval(
-        (() => {
-          this.setState({
-            currentImage: currentImage === 0 ? 1 : 0,
-            intervalId,
-          });
-        })(),
-        5000,
-      );
-    }
+  componentDidMount() {
+    const { currentImage } = this.state;
+    const intervalId = setInterval(this.timer, 5000);
+    setInterval(
+      (() => {
+        this.setState({
+          currentImage: currentImage === 0 ? 1 : 0,
+          intervalId,
+        });
+      })(),
+      5000,
+    );
+  }
 
-    componentWillUnmount() {
-      const { intervalId } = this.state;
-      clearInterval(intervalId);
-    }
+  componentWillUnmount() {
+    const { intervalId } = this.state;
+    clearInterval(intervalId);
+  }
 
-      timer = () => {
-        const { currentImage } = this.state;
-        this.setState({ currentImage: currentImage === 0 ? 1 : 0 });
-      };
+  timer = () => {
+    const { currentImage } = this.state;
+    this.setState({ currentImage: currentImage === 0 ? 1 : 0 });
+  };
 
-      render() {
-        const { currentImage } = this.state;
-        return (
-          <div className="landing">
-            <img
-              src={images[currentImage]}
-              className="landing__img"
-              alt="landing-img"
-            />
-          </div>
-        );
-      }
+  render() {
+    const { currentImage } = this.state;
+    return (
+      <div className="landing">
+        <div className="gradient">
+          <img
+            src={images[currentImage]}
+            className="landing__img"
+            alt="landing-img"
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Landing;
