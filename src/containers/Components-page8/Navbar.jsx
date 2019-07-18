8326aa2074be2e9dbde2c0b53c35fb18de301b84
roxioanes <roxioanes11@gmail.com>
2019-07-18 14:47:26 +0200

    Add navbar styling

--- src/containers/Components-page8/Navbar.jsx
+++ src/containers/Components-page8/Navbar.jsx
@@ -1,10 +1,108 @@
-import React from "react";
+import React, { Component } from "react";
+import Scrollspy from "react-scrollspy";
+import PropTypes from "prop-types";
 
-const Navbar = () => (
-  <div className="navbar">
-  
-  </div>
+class Navbar extends Component {
+  state = {
+    showMenu: false,
+  };
 
-);
+  componentDidMount() {
+    document.querySelector("html").classList.add("smooth-scroll");
+  }
+
+  componentWillUnmount() {
+    document.querySelector("html").classList.remove("smooth-scroll");
+  }
+
+  showMenu = (event) => {
+    event.preventDefault();
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener("click", this.closeMenu);
+    });
+  };
+
+  closeMenu = () => {
+    this.setState({ showMenu: false }, () => {
+      document.removeEventListener("click", this.closeMenu);
+    });
+  };
+
+  render() {
+    const { showMenu } = this.state;
+    const { scrolled } = this.props;
+    return (
+      <div className={scrolled ? "scrolled" : "navbar"}>
+        <div className={scrolled ? "scrolled__brand-wrapper" : "navbar__brand-wrapper"}>
+          <a href="#top" className={scrolled ? "scrolled__brand" : "navbar__brand"}>LOOKUP</a>
+        </div>
+        <div className="navbar__right-side">
+          <button type="button" className="nav-btn" onClick={this.showMenu}>
+            {showMenu ? (
+              <i className="fas fa-times nav-icon" />
+            ) : (
+              <i className="fas fa-bars nav-icon" />
+            )}
+          </button>
+        </div>
+        <nav className={scrolled ? "scrolled__list" : "navbar__list"}>
+          <Scrollspy
+            items={[
+              "home", "services", "about", "gallery", "our team", "testimonials", "contact",
+            ]}
+            currentClassName={scrolled ? "scrolled__is-current" : "navbar__is-current"}
+          >
+            {[
+              { value: "Home", href: "#home" },
+              { value: "Services", href: "#services" },
+              { value: "About", href: "#about" },
+              { value: "Gallery", href: "#gallery" },
+              { value: "Our Team", href: "#ourteam" },
+              { value: "Testimonials", href: "#testimonials" },
+              { value: "Contact", href: "#contact" },
+            ].map(navElement => (
+              <a href={navElement.href} key={navElement.value} className={scrolled ? "scrolled__list-item" : "navbar__list-item"}>
+                {navElement.value}
+              </a>
+            ))}
+          </Scrollspy>
+        </nav>
+        <nav className={`menu ${showMenu ? "dropdown-menu" : "closed-menu"}`}>
+          <Scrollspy
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "15px 0",
+              alignItems: "center",
+            }}
+            items={["home", "services", "about", "gallery", "our team", "testimonials", "contact"]}
+            currentClassName="dropdown-menu__is-current"
+          >
+            {[
+              { value: "Home", href: "#home" },
+              { value: "Services", href: "#services" },
+              { value: "About", href: "#about" },
+              { value: "Gallery", href: "#gallery" },
+              { value: "Our Team", href: "#ourteam" },
+              { value: "Testimonials", href: "#testimonials" },
+              { value: "Contact", href: "#contact" },
+            ].map(navElement => (
+              <a
+                href={navElement.href}
+                key={navElement.value}
+                className="dropdown-menu__list-item"
+              >
+                {navElement.value}
+              </a>
+            ))}
+          </Scrollspy>
+        </nav>
+      </div>
+    );
+  }
+}
+Navbar.propTypes = {
+  scrolled: PropTypes.bool.isRequired,
+};
 
 export default Navbar;
--- src/containers/Page8.jsx
+++ src/containers/Page8.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { Navbar } from "./Components-page8";
 import "../styles/page8.scss";
+import "../styles/page8-navbar.scss";
 
-const Page8 = () => (
-    <div className="page8">
-      <div className="container">
-        <h3>wee</h3>
+class Page8 extends Component {
+  state= {
+    scrolled: false,
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.scrollFunction, true);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.scrollFunction, true);
+  }
+
+  scrollFunction = () => {
+    const isTop = window.scrollY < 50;
+    if (isTop !== true) {
+      this.setState({ scrolled: true });
+    } else {
+      this.setState({ scrolled: false });
+    }
+  }
+
+  render() {
+    const { scrolled } = this.state;
+    return (
+      <div className="page8">
+        <div className="container">
+          <Navbar scrolled={scrolled} />
+        </div>
       </div>
-    </div>
-
-);
-
+    );
+  }
+}
 export default Page8;
