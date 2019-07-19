e90316b8bc6ff5bea41a58b7a071c83c845de808
roxioanes <roxioanes11@gmail.com>
2019-07-19 12:38:31 +0200

    Add landing text part

--- src/containers/Components-page8/Landing.jsx
+++ src/containers/Components-page8/Landing.jsx
@@ -5,55 +5,56 @@ import image2 from "../images/landing82.jpg";
 
 const images = [image1, image2];
 const texts = [{
-    word1: "Enrich",
-    word2: "Luxurious",
+  word1: "Enrich",
+  word2: "Luxurious",
 }, {
-    word1: "Elegant",
-    word2: "Comfortable",
+  word1: "Elegant",
+  word2: "Comfortable",
 }];
 
-class Landing extends Component{
+class Landing extends Component {
     state = {
-        currentOption: 0,
+      currentOption: 0,
     };
 
     componentDidMount() {
-        const { currentOption } = this.state;
-        const intervalId = setInterval(this.timer, 5000);
-        setInterval(
-          (() => {
-            this.setState({
-              currentOption: currentOption === 0 ? 1 : 0,
-              intervalId,
-            });
-          })(),
-          5000,
-        );
-      }
-    
-      componentWillUnmount() {
-        const { intervalId } = this.state;
-        clearInterval(intervalId);
-      }
-    
+      const { currentOption } = this.state;
+      const intervalId = setInterval(this.timer, 5000);
+      setInterval(
+        (() => {
+          this.setState({
+            currentOption: currentOption === 0 ? 1 : 0,
+            intervalId,
+          });
+        })(),
+        5000,
+      );
+    }
+
+    componentWillUnmount() {
+      const { intervalId } = this.state;
+      clearInterval(intervalId);
+    }
+
       timer = () => {
         const { currentOption } = this.state;
         this.setState({ currentOption: currentOption === 0 ? 1 : 0 });
       };
 
-    render(){
+      render() {
         const { currentOption } = this.state;
-        return(
-            <div className="landing">
-              <div class="landing__images-switch">
-               <img src={images[currentOption]} className="landing__img" alt="landing-img" />
-                <h3 className="landing__word1">{texts[currentOption].word1}</h3>
-                <h3 className="landing__word2">{texts[currentOption].word2}</h3>
-              </div>
-               
+        return (
+          <div className="landing">
+            <img src={images[currentOption]} className="landing__img" alt="landing-img" />
+            <h3 className="landing__word1">{texts[currentOption].word1}</h3>
+            <h3 className="landing__word2">{texts[currentOption].word2}</h3>
+            <div className="landing__text-wrapper">
+              <h3 className="landing__title">Interior Designers and Decorators</h3>
+              <p className="landing__p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu libero scelerisque ligula sagittis faucibus eget quis lacus.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
+          </div>
         );
-    }
+      }
 }
 
 export default Landing;
