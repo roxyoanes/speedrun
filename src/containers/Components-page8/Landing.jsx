4693d246d2eb4b819811894ad14fb1b22c943301
roxioanes <roxioanes11@gmail.com>
2019-08-02 12:18:19 +0200

    Add buttons left/right on landing img

--- src/containers/Components-page8/Landing.jsx
+++ src/containers/Components-page8/Landing.jsx
@@ -12,10 +12,22 @@ const texts = [{
   word2: "Comfortable",
 }];
 
+const pickImage = (number) => {
+  switch (number) {
+    case 1:
+      return image1;
+    case 2:
+      return image2;
+    default:
+      return image1;
+  }
+};
+
 class Landing extends Component {
   state = {
     currentOption: 0,
   };
+
 
   componentDidMount() {
     const intervalId = setInterval(this.timer, 5000);
@@ -29,6 +41,18 @@ class Landing extends Component {
     clearInterval(intervalId);
   }
 
+  changeNumber = () => {
+    const { currentOption } = this.state;
+    this.setState(
+      { currentOption: currentOption === 0 ? 1 : 0 },
+      // we need the new state after updating it on line 47
+      // currentOption defined in line 45 still holds the old one
+      //
+      // eslint-disable-next-line react/destructuring-assignment
+      () => pickImage(this.state.currentOption),
+    );
+  };
+
   timer = () => {
     const { currentOption } = this.state;
     this.setState({ currentOption: currentOption === 0 ? 1 : 0 });
@@ -41,6 +65,20 @@ class Landing extends Component {
         <img src={images[currentOption]} className="landing__img" alt="landing-img" />
         <h3 className="landing__word1">{texts[currentOption].word1}</h3>
         <h3 className="landing__word2">{texts[currentOption].word2}</h3>
+        <button
+          type="button"
+          className="button-left"
+          onClick={() => this.changeNumber()}
+        >
+          <span className="fas fa-arrow-circle-left icon-btn" />
+        </button>
+        <button
+          type="button"
+          className="button-right"
+          onClick={() => this.changeNumber()}
+        >
+          <span className="fas fa-arrow-circle-right icon-btn" />
+        </button>
         <div className="landing__text-wrapper">
           <h3 className="landing__title">Interior Designers and Decorators</h3>
           <p className="landing__p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu libero scelerisque ligula sagittis faucibus eget quis lacus.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
