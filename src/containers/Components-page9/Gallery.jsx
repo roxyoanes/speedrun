ee3c8a2659b99f74078d71f1956b6fe89491e2b2
roxioanes <roxioanes11@gmail.com>
2019-08-09 11:41:09 +0200

    Add pictures for gallery

--- src/containers/Components-page9/Gallery.jsx
+++ src/containers/Components-page9/Gallery.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
+import img1 from "../images/portfolio_pic1.jpg";
+import img2 from "../images/portfolio_pic2.jpg";
+import img3 from "../images/portfolio_pic3.jpg";
+import img4 from "../images/portfolio_pic4.jpg";
+import img5 from "../images/portfolio_pic5.jpg";
+import img6 from "../images/portfolio_pic6.jpg";
+import img7 from "../images/portfolio_pic7.jpg";
+import img8 from "../images/portfolio_pic8.jpg";
+
+
 const Gallery = () => (
   <div className="gallery">
     <h2 className="gallery__title">Projects</h2>
