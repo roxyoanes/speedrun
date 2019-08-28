18f8858d5e19fb22541495116cc098a0bf6e8784
roxioanes <roxioanes11@gmail.com>
2019-08-28 09:39:24 +0200

    Add images screenshot section

--- src/containers/Components-page10/Screenshots.jsx
+++ src/containers/Components-page10/Screenshots.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Screenshots = () => (
   <div className="screenshots">
-    <h2 className="features__title">Screenshots</h2>
+    <div className="screenshots__title-wrapper">
+      <h2 className="screenshots__title">Screenshots</h2>
+      <div className="screenshots__oneline" />
+    </div>
   </div>
 );
 
