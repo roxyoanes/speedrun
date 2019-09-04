564d238534920587ad2ef11618979c02219506f3
roxioanes <roxioanes11@gmail.com>
2019-09-04 11:28:36 +0200

    Add style footer

--- src/containers/Components-page10/Footer.jsx
+++ src/containers/Components-page10/Footer.jsx
@@ -2,22 +2,20 @@ import React from "react";
 
 const Footer = () => (
   <div className="footer">
-    <div className="footer__wrapper">
-      <span className="footer__icons">
-        <a href="https://twitter.com/" className="download__icon-wrapper">
-          <i className="fab fa-twitter icon" />
-        </a>
-        <a href="https://facebook.com/" className="download__icon-wrapper">
-          <i className="fab fa-facebook-f icon" />
-        </a>
-        <a href="https://plus.google.com/" className="download__icon-wrapper">
-          <i className="fab fa-google-plus-g icon" />
-        </a>
-        <a href="https://instagram.com/" className="download__icon-wrapper">
-          <i className="fab fa-instagram icon" />
-        </a>
-      </span>
-    </div>
+    <span className="footer__icons">
+      <a href="https://twitter.com/" className="footer__icon-wrapper">
+        <i className="fab fa-twitter icon" />
+      </a>
+      <a href="https://facebook.com/" className="footer__icon-wrapper">
+        <i className="fab fa-facebook-f icon" />
+      </a>
+      <a href="https://plus.google.com/" className="footer__icon-wrapper">
+        <i className="fab fa-google-plus-g icon" />
+      </a>
+      <a href="https://instagram.com/" className="footer__icon-wrapper">
+        <i className="fab fa-instagram icon" />
+      </a>
+    </span>
     <h5 className="footer__text">
      © 2018 Company Name. Template by
       <a className="footer__link" href="http://webthemez.com/interior-design/">WebThemez.com</a>
