2271b3b1f3de3694fcb7bffea49df6db0af33aff
roxioanes <roxioanes11@gmail.com>
2019-10-01 15:29:00 +0200

    Add style landing part2

--- src/containers/Components-page11/Landing.jsx
+++ src/containers/Components-page11/Landing.jsx
@@ -7,6 +7,15 @@ const Landing = () => (
       <h3 className="landing__subtitle">Yes - Start, run and grow your business. Eos debitis prominus.</h3>
       <button className="btn-landing" type="button">READ MORE</button>
     </div>
+    <div className="landing__part2">
+      <h2 className="landing__title2">
+        welcome to
+        <strong> StartUp - </strong>
+        <span className="landing__word-color">Start</span>
+        , run and grow your business.
+      </h2>
+      <p className="landing__p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure aperiam consequatur quo. Sed quis tortor magna. Maecenas hendrerit feugiat pulvinar. Aenean condimentum quam eu ultricies cursus. Nulla facilisi. In hac habitasse platea dictumst. Ut nec tellus neque. Sed non dui eget arcu elementum facilisis.</p>
+    </div>
   </div>
 );
 
